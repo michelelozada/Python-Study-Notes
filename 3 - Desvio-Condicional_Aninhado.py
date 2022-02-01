e88a263487baeffa41f0d4cc956484884719b700
Michele Lozada <michelelozada1@gmail.com>
2022-02-01 19:18:28 -0300

    Files added

--- 3 - Desvio-Condicional_Aninhado.py
+++ 3 - Desvio-Condicional_Aninhado.py
@@ -1,3 +1,10 @@
+'''
+Escreva um algoritmo para servir de pré-atendimento para encomenda de clientes em uma panificadora.
+Um menu deve oferecer as opções disponíveis para encomenda e qual a quantidade que o cliente deseja encomendar.
+A saída deve apresentar o valor total a pagar de tal encomenda.
+'''
+
+
 print('Panificadora Bela Vista - Auto-atendimento')
 print('--------------------------------------------')
 print('\nConfira abaixo nossas opções para hoje:')
@@ -31,3 +38,22 @@ else:
           print(f'\n> Você encomendou {encomenda} broa(s) de trigo integral, sendo que o valor de seu pedido ficou em R$ {total:.2f} |')
         else:
           print('> Esta opção é inválida.')
+
+  '''
+A saída gerada: 
+
+Panificadora Bela Vista - Auto-atendimento
+--------------------------------------------
+
+Confira abaixo nossas opções para hoje:
+1 - Broa de de aveia: R$ 7.50
+2 - Broa de centeio: R$ 7.00
+3 - Broa de fubá: R$ 6.50
+4 - Broa de milho: R$ 8.00
+5 - Broa de trigo integral: R$ 8.50
+
+Digite o número da opção escolhida: 3
+Quantas broas deste sabor deseja encomendar? 4
+
+> Você encomendou 4 broa(s) de fubá, sendo que o valor de seu pedido ficou em R$ 26.00
+  '''
