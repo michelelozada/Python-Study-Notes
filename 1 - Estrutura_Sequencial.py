6567f2732dedb49270cf83d426f7c9af00d396fb
Michele Lozada <michelelozada1@gmail.com>
2022-02-02 16:58:49 -0300

    Added files

--- 1 - Estrutura_Sequencial.py
+++ 1 - Estrutura_Sequencial.py
@@ -5,12 +5,12 @@ O algoritmo deve receber o valor da cotação da libra e a quantidade em reais a
 
 print('- Conversor de Real Brasileiro (BRL) para Libra Esterlina (GBP) -')
 
-cotacaoReal = float(input('Qual o valor da cotação da Libra Esterlina em relação ao Real hoje? £ 1.00 = R$ '))
-quantidadeReal = float(input('Qual o valor que você deseja converter para Libras Esterlinas? R$ ' ))
+cotacaoLibra = float(input('Qual o valor da cotação da Libra Esterlina em relação ao Real hoje? £ 1.00 = R$ '))
+valorReal = float(input('Qual o valor que você deseja converter para Libras Esterlinas? R$ ' ))
 
-quantidadeLibras =  quantidadeReal/cotacaoReal
+valorLibras = valorReal/cotacaoLibra
 
-print(f'\n> Resultado da conversão de moedas: R$ {quantidadeReal:.2f} (BRL) = £ {quantidadeLibras:.4f} (GBP) ')
+print(f'\n> Resultado da conversão de moedas: R$ {valorReal:.2f} (BRL) = £ {valorLibras:.4f} (GBP) ')
 
 
 '''
