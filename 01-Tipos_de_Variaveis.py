309cf23346089aedce4475fc9da2b977034ba6e3
Michele Lozada <michelelozada1@gmail.com>
2022-03-08 14:28:07 -0300

    Edited file

---
+++ 01-Tipos_de_Variaveis.py
@@ -0,0 +1,47 @@
+# Diga quais são os tipos das variáveis aparesentadas abaixo:
+
+#1
+a = 7
+
+# 2
+b = 10.5
+
+#3
+c = 3 + 4j
+
+# 4
+d = 'Tipos de variáveis utilizadas em Python!'
+
+# 5
+e = True
+
+# 6
+f = ['a', 2, 'c', 4, 5]
+
+# 7
+g = ('Paraná', 'Santa Catarina','Rio Grande do Sul')
+
+# 8
+h = {'Nome': 'Davi', "Idade": 18, 'Profissão':'Programador'}
+
+#Retornando o tipo das variáveis acima
+print('1 - Variável a:',a,'->', type(a))
+print('2 - Variável b:',b,'->', type(b))
+print('3 - Variável c:',c,'->', type(c))
+print('4 - Variável d:',d,'->', type(d))
+print('5 - Variável e:',e,'->', type(e))
+print('6 - Variável f:',f,'->', type(f))
+print('7 - Variável g:',g,'->', type(g))
+print('8 - Variável h:',h,'->', type(h))
+
+
+'''
+1 - Variável a: 7 -> <class 'int'>
+2 - Variável b: 10.5 -> <class 'float'>
+3 - Variável c: (3+4j) -> <class 'complex'>
+4 - Variável d: Tipos de variáveis utilizadas em Python! -> <class 'str'>
+5 - Variável e: True -> <class 'bool'>
+6 - Variável f: ['a', 2, 'c', 4, 5] -> <class 'list'>
+7 - Variável g: ('Paraná', 'Santa Catarina', 'Rio Grande do Sul') -> <class 'tuple'>
+8 - Variável h: {'Nome': 'Davi', 'Idade': 18, 'Profissão': 'Programador'} -> <class 'dict'>
+'''
