c6c7531d461024d64efa056c5a6609ce48494503
Michele Lozada <michelelozada1@gmail.com>
2022-03-20 10:14:31 -0300

    Edited file

--- 14-Dicionarios.py
+++ 14-Dicionarios.py
@@ -1,3 +1,4 @@
+# 1 - Estrutura inicial
 ficha_livro = {
     'Título': 'Harry Potter e a Pedra Filosofal',
     'Título original': 'Harry Potter and the Philosopher\'s Stone',
@@ -7,28 +8,28 @@ ficha_livro = {
 }
 
 
-# Retorno do tipo da estrutura de dados acima
+# 2 - Retorno do tipo da estrutura de dados acima
 print(type(ficha_livro))
 # <class 'dict'>
 
 
-# Impressão do dicionário
+# 3 - Impressão do dicionário
 print(ficha_livro)
-# {'Título': 'Harry Potter e a Pedra Filosofal', 'Título original': "Harry Potter and the Philosopher's Stone",
-# 'Autor(a)': 'Rowling, J.K.', 'Editora': 'Rocco', 'Páginas': '264 páginas'}
+# {'Título': 'Harry Potter e a Pedra Filosofal', 'Título original': "Harry Potter and the Philosopher's Stone", 'Autor(a)': 'Rowling, J.K.', 'Editora': 'Rocco', 'Páginas': '264 páginas'}
 
 
-# Acessar o valor do índice correspondente à editora do livro
+# 4 - Acesso do valor do índice correspondente à editora do livro
 print(ficha_livro['Editora'])
 # Rocco
 
 
-# Adição de uma nova chave e valor
+# 5 - Adição de uma nova chave e valor
 ficha_livro['Ano de publicação'] = 2000
 print(ficha_livro)
 # {'Título': 'Harry Potter e a Pedra Filosofal', 'Título original': "Harry Potter and the Philosopher's Stone", 'Autor(a)': 'Rowling, J.K.', 'Editora': 'Rocco', 'Páginas': '264 páginas', 'Ano de publicação': 2000}
 
-# Criação um novo dicionário independente baseado no primeiro, seguido de alteração de valores.
+
+# 6 - Criação de um dicionário independente baseado no primeiro, seguido da alteração dos valores abaixo
 ficha_livro2 = ficha_livro.copy()
 ficha_livro2['Título'] = 'Harry Potter e o Prisioneiro de Azkaban'
 ficha_livro2['Título original'] = 'Harry Potter and the Prisoner of Azkaban'
@@ -36,3 +37,31 @@ ficha_livro2['Páginas']= '288 páginas'
 ficha_livro2['Ano de publicação'] = 2007
 print(ficha_livro2)
 #{'Título': 'Harry Potter e o Prisioneiro de Azkaban', 'Título original': 'Harry Potter and the Prisoner of Azkaban', 'Autor(a)': 'Rowling, J.K.', 'Editora': 'Rocco', 'Páginas': '288 páginas', 'Ano de publicação': 2007}
+
+
+# 7 - Para ambas as listas, inclusão da informação de que se trata da 1ª ediição dos livros.
+edition ={
+    'Edição': '1ª ed.',
+}
+ficha_livro.update(edition)
+ficha_livro2.update(edition)
+print(ficha_livro)
+print(ficha_livro2)
+# {'Título': 'Harry Potter e a Pedra Filosofal', 'Título original': "Harry Potter and the Philosopher's Stone", 'Autor(a)': 'Rowling, J.K.', 'Editora': 'Rocco', 'Páginas': '264 páginas', 'Ano de publicação': 2000, 'Edição': '1ª ed.'}
+# {'Título': 'Harry Potter e o Prisioneiro de Azkaban', 'Título original': 'Harry Potter and the Prisoner of Azkaban', 'Autor(a)': 'Rowling, J.K.', 'Editora': 'Rocco', 'Páginas': '288 páginas', 'Ano de publicação': 2007, 'Edição': '1ª ed.'}
+
+
+# 8 - Considerando-se somente o primeiro dicionário:
+# 8.1 - Retorno de uma lista apenas com as chaves do dicionário
+print(ficha_livro.keys())
+# dict_keys(['Título', 'Título original', 'Autor(a)', 'Editora', 'Páginas', 'Ano de publicação', 'Edição'])
+
+
+# 8.2 - Retorno de uma lista apenas com os valores do dicionário
+print(ficha_livro.values())
+# dict_values(['Harry Potter e a Pedra Filosofal', "Harry Potter and the Philosopher's Stone", 'Rowling, J.K.', 'Rocco', '264 páginas', 2000, '1ª ed.'])
+
+
+# 8.3. - Retorno de uma lista com os pares chave-valor do dicionário
+print(ficha_livro.items())
+#dict_items([('Título', 'Harry Potter e a Pedra Filosofal'), ('Título original', "Harry Potter and the Philosopher's Stone"), ('Autor(a)', 'Rowling, J.K.'), ('Editora', 'Rocco'), ('Páginas', '264 páginas'), ('Ano de publicação', 2000), ('Edição', '1ª ed.')])
