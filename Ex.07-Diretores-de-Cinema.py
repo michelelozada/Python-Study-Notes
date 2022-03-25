511297c7ee11b1071a469ac3c1b56344b234b21b
Michele Lozada <michelelozada1@gmail.com>
2022-03-25 11:22:36 -0300

    Edited file

--- Ex.07-Diretores-de-Cinema.py
+++ Ex.07-Diretores-de-Cinema.py
@@ -1,32 +1,25 @@
 """
 Foi publicada uma lista com os diretores mais influentes de todos os tempos em Hollywood:
-1º. Steven Spielberg*
-2º. Stanley Kubrick
-3º. Bernardo Bertolucci
-4º. Jean-Luc Godard
-5º. Quentin Tarantino*
-6º. Martin Scorsese*
-7º. Alfred Hitchcock
-8º. Tim Burton*
-9º. Woody Allen*
-10º. Francis Ford Coppola*
-11º. George Lucas*
-12º. Charles Chaplin
-13º. Ingmar Bergman
-14º. James Cameron*
-15º. Federico Fellini
-(*) Indica os diretores vivos.
-
-Portanto:
-A - Liste os 10 dos diretores de cinema mais influentes de todos os tempos. Imprima  essa lista na ordem decrescente.
-B - Inclua nela os 5 nomes subsequentes. Agora a lista deve ser impressa na ordem crescente.
-B - Crie um algoritmo que responda se os cineastas Woody Allen e Fran Capra estão nesta lista. Se a resposta for positiva,
-deve dizer qual a posição do diretor na lista.
-D - Por fim, a lista deve mostar somente os cinco maiores diretores vivos.
+1º. Steven Spielberg
+2º. Stanley Kubrick*
+3º. Bernardo Bertolucci*
+4º. Jean-Luc Godard*
+5º. Quentin Tarantino
+6º. Martin Scorsese
+7º. Alfred Hitchcock*
+8º. Tim Burton
+9º. Woody Allen
+10º. Francis Ford Coppola
+11º. George Lucas
+12º. Charles Chaplin*
+13º. Ingmar Bergman*
+14º. James Cameron
+15º. Federico Fellini*
+(*) Indica os diretores já falecidos
 """
 
 
-# A
+# A - Liste os 10 dos diretores de cinema mais influentes de todos os tempos. Imprima  essa lista na ordem decrescente.
 maiores_diretores = [
     'Steven Spielberg',
     'Stanley Kubrick',
@@ -39,18 +32,16 @@ maiores_diretores = [
     'Woody Allen',
     'Francis Ford Coppola'
 ]
-
 print(f'Lista dos 10 diretores mais influentes de Hollywood:\n{maiores_diretores[::-1]}.')
 
 """
 Output:
-Lista dos 10 diretores mais influentes de Hollywood:
-['Francis Ford Coppola', 'Woody Allen', 'Tim Burton', 'Alfred Hitchcock', 'Martin Scorsese', 'Quentin Tarantino', 
-'Jean-Luc Godard', 'Bernardo Bertolucci', 'Stanley Kubrick', 'Steven Spielberg'].
+Lista dos 10 diretores mais influentes de Hollywood: ['Francis Ford Coppola', 'Woody Allen', 'Tim Burton', 'Alfred Hitchcock', 
+'Martin Scorsese', 'Quentin Tarantino', 'Jean-Luc Godard', 'Bernardo Bertolucci', 'Stanley Kubrick', 'Steven Spielberg'].
 """
 
 
-#B
+# B - Inclua nela os 5 nomes subsequentes. Agora a lista deve ser impressa na ordem crescente.
 maiores_diretores.extend([
     'George Lucas',
     'Charles Chaplin',
@@ -69,48 +60,48 @@ Lista dos 15 diretores mais influentes de Hollywood:
 """
 
 
-# C
-sim = 'está na lista dos 10 maiores diretores de todos os tempos.'
-nao = 'não está na lista dos 10 maiores diretores de todos os tempos.'
-diretor = input('\nQual o nome do diretor? ')
+# C - Crie um algoritmo que responda se os cineastas Woody Allen e Fran Capra estão nesta lista.
+# Se a resposta for positiva, deve informar qual o número do índice que o diretor ocupa na lista acima dos diretores.
+consulta = [
+    'Woody Allen',
+    'Frank Capra'
+]
+sim = 'está'
+nao = 'não está'
+msg = 'na lista dos 10 maiores diretores mais influentes de todos os tempos.'
 
-if diretor in maiores_diretores:
-    print('%s %s'%(diretor,sim))
-    print(f'Ele ocupa o índice de nº {maiores_diretores.index(diretor)} na lista.')
-else:
-    print('> %s %s' %(diretor,nao))
+for diretor in consulta:
+    if diretor in maiores_diretores:
+        print('\n> %s %s %s' % (diretor, sim, msg), end=' ')
+        print(f'Ele ocupa o índice de nº {maiores_diretores.index(diretor)} na lista.')
+    else:
+        print('> %s %s %s' % (diretor, nao, msg))
 
 """
 Output:
-Qual o nome do diretor? Woody Allen
-    Woody Allen está na lista dos 10 maiores diretores de todos os tempos.
-    Ele ocupa o índice de nº 8 na lista.
-
-Qual o nome do diretor? Frank Capra
-    Frank Capra não está na lista dos 10 maiores diretores de todos os tempos.
+> Woody Allen está na lista dos 10 maiores diretores mais influentes de todos os tempos. Ele ocupa o índice de nº 8 na lista.
+> Frank Capra não está na lista dos 10 maiores diretores mais influentes de todos os tempos.
 """
 
 
-# D
-if 'Stanley Kubrick' in maiores_diretores:
-    maiores_diretores.remove('Stanley Kubrick')
-if 'Bernardo Bertolucci' in maiores_diretores:
-    maiores_diretores.remove('Bernardo Bertolucci')
-if 'Jean-Luc Godard' in maiores_diretores:
-    maiores_diretores.remove('Jean-Luc Godard')
-if 'Alfred Hitchcock' in maiores_diretores:
-    maiores_diretores.remove('Alfred Hitchcock')
-if 'Charles Chaplin' in maiores_diretores:
-    maiores_diretores.remove('Charles Chaplin')
-if 'Ingmar Bergman' in maiores_diretores:
-    maiores_diretores.remove('Ingmar Bergman')
-if 'Federico Fellini' in maiores_diretores:
-    maiores_diretores.remove('Federico Fellini')
+# D - Apresente a lista agora com apenas os cinco maiores diretores vivos.
+deceased_directors =[
+    'Stanley Kubrick',
+    'Bernardo Bertolucci',
+    'Jean-Luc Godard',
+    'Alfred Hitchcock',
+    'Charles Chaplin',
+    'Ingmar Bergman',
+    'Federico Fellini'
+]
 
+for diretor in deceased_directors:
+    if diretor in maiores_diretores:
+        maiores_diretores.remove(diretor)
 print(f'\nLista dos 5 diretores vivos mais influentes de Hollywood:\n{maiores_diretores[0:5]}.')
 
 """
 Output:
 Lista dos 5 diretores vivos mais influentes de Hollywood:
-['Steven Spielberg', 'Quentin Tarantino', 'Martin Scorsese', 'Tim Burton', 'Woody Allen']
+['Steven Spielberg', 'Quentin Tarantino', 'Martin Scorsese', 'Tim Burton', 'Woody Allen'].
 """
