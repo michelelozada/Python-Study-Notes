56445dfc11c7ef2a1bb441b122a56e6be08bfbd9
Michele Lozada <michelelozada1@gmail.com>
2022-04-06 07:58:21 -0300

    Added file

---
+++ 18-Escopo.py
@@ -0,0 +1,30 @@
+# 1 - Escopo global vs. escopo local
+
+x = 'global' # variável de escopo global
+def minhaFuncao():
+    x = 'local' # varável de escopo local
+    print(x)
+
+# Evocando a função
+minhaFuncao()
+# Print da variável x
+print(x)
+
+''' Output:
+local
+global
+'''
+
+# 2 - Utlização da global keyword
+x = 'global'
+def minhaFuncao():
+    global x
+    x = 'local'
+    print(x)
+
+minhaFuncao()
+print(x)
+''' Output:
+local 
+local
+'''
